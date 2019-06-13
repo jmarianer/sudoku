906abffdd4ce6c177e76e56784863b396b3bc83a
Joey Marianer <rmarianer@instructure.com>
2019-06-13 12:30:27 -0700

    Add region borders. The display code is, unfortunately, hard-coded to 9x9, even though the rest of the code is not

--- templates/board.tsx
+++ templates/board.tsx
@@ -2,7 +2,8 @@ import { Region, Cell, Board } from '../types';
 import * as React from './noreact';
 
 const possCells: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-function cellToTd(cell: Cell, className: string) {
+function cellToTd(cell: Cell, className: string, r: number, c: number) {
+  className += " r" + r + " c" + c
   if (cell.possibilities.size == 1) {
     return <td class={ className }>{Array.from(cell.possibilities.values())[0]}</td>
   }
@@ -24,10 +25,12 @@ const boardCells: number[][] = range9.map((i)=>range9.map((j)=>i*9+j));
 
 export = (board: Board, region?: Region) =>
   <table class="sudoku">
-  { boardCells.map((row) =>
+  { boardCells.map((row, r) =>
     <tr>
-    { row.map((index) =>
-      cellToTd(board.cells[index], (region && region.cell_indexes.has(index)) ? "highlighted" : "")) }
+    { row.map((index, c) =>
+      cellToTd(board.cells[index],
+        (region && region.cell_indexes.has(index)) ? "highlighted" : "",
+        r, c)) }
     </tr>
   )}
   </table>;
